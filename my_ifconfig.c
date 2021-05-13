d439b02d35d4c35f14650ab0b3284981e740f79c
Swapnil Kulkarni <swapnilvamankulkarni@gmail.com>
2021-05-14 00:52:24 +0530

    cleaned-up code a bit

--- my_ifconfig.c
+++ my_ifconfig.c
@@ -5,142 +5,151 @@
 #include <net/if.h>
 #include <sys/ioctl.h>
 
+#define MAC_SIZE 6
 
- void Get_all_interfaces_names(struct ifreq *ifr, short int total_interfaces);
- short int Print_info (struct ifreq *ifr, int argc, char *argv[], int total_interfaces);
- short int Get_info (struct ifreq *ifr, short int total_interfaces, int argc, char *argv[]);
+struct interfaces{
+		
+		unsigned char name[IFNAMSIZ];
+		unsigned char hwaddr[MAC_SIZE];
+		int mtu;
+	};
 
-char path[100], interfaces;
+void get_all_interfaces_names(struct interfaces *ifc, short int count, unsigned char *interface_names);
+short int print_info (struct interfaces *ifc, unsigned char *port_name, int count);
+short int get_info (unsigned char *port_name);
+
+
+
 
 
 int main(int argc, char *argv[])
 {
-	FILE *fp,*fp2;
+	
+	 unsigned char port_name [IFNAMSIZ];
 	 
-	short int  total_interfaces ,pos,i=0;
-	
-	fp=popen("ls /sys/class/net","r"); //to get names of the network interfaces present in the system
-	
-	if (fp==NULL)
-	 perror("couldnt get interface names\t");   
 	 
-	fread(path,sizeof(char),100,fp);   
-	
-	
-	fp2= popen("ls /sys/class/net | wc -l","r");  //to get total count of the network interfaces present in the system 
-	 
-	if (fp2==NULL)
-	 perror("couldnt get total interface count\t");
-	
-	fread(&interfaces,sizeof(char),1,fp2);
-	
-	 
-	 total_interfaces= interfaces - 48; // converting char to int
-	 	 
-	 struct ifreq ifr[total_interfaces];
-	
-	 //printf("content from the fread is ---->\n%s\ninterfaces=%d\n",path, total_interfaces);
-	 
-	
-	 Get_info (ifr, total_interfaces, argc, argv);
-	 Print_info(ifr, argc, argv, total_interfaces);
-	 
-	 //Get_interface_names (ifr, total_interfaces);
-	
-	pclose (fp);
-	pclose (fp2);
-	
+	 if(argc > 1) {  strncpy(port_name, argv[1], IFNAMSIZ); }
+	 else { strncpy(port_name, "no_port", IFNAMSIZ); }
+
+	 get_info(port_name );
+	 //print_info(port_name);
+
 	return 0;
 }
 
 
-short int Get_info (struct ifreq *ifr, short int total_interfaces, int argc, char *argv[])
+short int get_info (unsigned char *port_name)
 {
-	int fd;
-	fd = socket(AF_INET, SOCK_DGRAM, 0);
- 
-   if (fd < 0)
-   {
-     perror("EXITING the program...!!!couldnt open socket\t");
-     return 1;  
-   }
+	
+	int fd, i=0,count, count1;
+	unsigned char interface_count, interface_names[100];
+	FILE *fp,*fp2;
+	
+	
+	fp=popen("ls /sys/class/net","r"); //to get names of the network interfaces present in the system
+	
+	if(fp==NULL) { perror("couldnt get interface names\t"); }  
+	 
+	fread(interface_names,sizeof(char),100,fp);  
+	
+	
+	fp2= popen("ls /sys/class/net | wc -l","r");  //to get total count of the network interfaces present in the system 
+	 
+	if(fp2==NULL) { perror("couldnt get total interface count\t"); }
+	
+	fread(&interface_count,sizeof(char),1,fp2);
+
+   count1 = count = interface_count - 48; // converting char to int
+	struct interfaces ifc[count]; struct ifreq ifr[count];
+	 
+	fd = socket(AF_INET, SOCK_DGRAM, 0);  if (fd < 0) { perror("EXITING the program...!!!couldnt open socket\t"); return 1; }
     
-   total_interfaces=total_interfaces-1;
-   Get_all_interfaces_names(ifr, total_interfaces);
-   
-   for (total_interfaces; total_interfaces >= 0; --total_interfaces)
+   count=count-1; //as array index starts from 0
+   get_all_interfaces_names(ifc, count, interface_names);
+    
+
+   if(!(strcmp(port_name, "no_port")))
    {
-       if (ioctl(fd, SIOCGIFHWADDR, &ifr[total_interfaces])<0)
-       {
-          perror("EXITING the program...!!!couldnt get hardware address\t");
-          printf("--->%s\n", ifr[total_interfaces].ifr_name);
-          return 1;
-       }
- 
-       if (ioctl(fd, SIOCGIFMTU, &ifr[total_interfaces])<0)
-       {          
-          perror("EXITING the program...!!!couldnt get MTU\t");
-          return 1;
-       }  
-     /*   
-       printf("mtu of %s=%d\t hwdaddrs=%02X:%02X:%02X:%02x:%02x:%02x\n",ifr[total_interfaces].ifr_name,ifr[total_interfaces].ifr_mtu,
-       ifr[total_interfaces].ifr_hwaddr.sa_data[0],ifr[total_interfaces].ifr_hwaddr.sa_data[1],ifr[total_interfaces].ifr_hwaddr.sa_data[2],
-       (unsigned char)ifr[total_interfaces].ifr_hwaddr.sa_data[3],ifr[total_interfaces].ifr_hwaddr.sa_data[4],ifr[total_interfaces].ifr_hwaddr.sa_data[5]);
-     */
+	   for (count; count >= 0; count--)
+	   {
+	   	 strncpy(ifr[count].ifr_name,ifc[count].name, IFNAMSIZ);
+	   	 
+	       if (ioctl(fd, SIOCGIFHWADDR, &ifr[count])<0) { perror("EXITING the program...!!!couldnt get hardware address\t"); return 1; }
+			 memcpy(ifc[count].hwaddr, ifr[count].ifr_hwaddr.sa_data, MAC_SIZE);   		
+	   		
+	       if (ioctl(fd, SIOCGIFMTU, &ifr[count])<0) { perror("EXITING the program...!!!couldnt get MTU\t"); return 1; }  
+	       ifc[count].mtu=ifr[count].ifr_mtu;
+	       
+	   }
+   } 
+   
+   else 
+   {
+   	count = 0;
+   	strncpy(ifr[count].ifr_name, port_name, IFNAMSIZ);
+   
+   	 if (ioctl(fd, SIOCGIFHWADDR, &ifr[count])<0) { perror("EXITING the program...!!!couldnt get hardware address\t"); return 1; }
+		 memcpy(ifc[count].hwaddr, ifr[count].ifr_hwaddr.sa_data, MAC_SIZE);   		
+	   		
+	    if (ioctl(fd, SIOCGIFMTU, &ifr[count])<0) { perror("EXITING the program...!!!couldnt get MTU\t"); return 1; }  
+	    ifc[count].mtu=ifr[count].ifr_mtu;
+   
    }
-     
+   
+    print_info(ifc, port_name, interface_count-48);
+      
+   pclose(fp);
+	pclose(fp2);   
    close(fd);
 
 }
 
-short int Print_info (struct ifreq *ifr, int argc, char *argv[], int total_interfaces)
+
+
+
+short int print_info (struct interfaces *ifc, unsigned char *port_name, int count)
 {
- 
- if (argc<=1)
- {
-   for (--total_interfaces; total_interfaces >= 0; --total_interfaces)
+	
+ count=count-1;
+ if(!(strcmp(port_name, "no_port")))
+ {	
+   for (count; count >= 0; count--)
    {
         
-    printf("mtu of %s=%d\t hwdaddrs=%02X:%02X:%02X:%02x:%02x:%02x\n",ifr[total_interfaces].ifr_name,ifr[total_interfaces].ifr_mtu,
-    (unsigned char)ifr[total_interfaces].ifr_hwaddr.sa_data[0], (unsigned char)ifr[total_interfaces].ifr_hwaddr.sa_data[1], (unsigned char)ifr[total_interfaces].ifr_hwaddr.sa_data[2],
-    (unsigned char)ifr[total_interfaces].ifr_hwaddr.sa_data[3], (unsigned char)ifr[total_interfaces].ifr_hwaddr.sa_data[4], (unsigned char)ifr[total_interfaces].ifr_hwaddr.sa_data[5]);
+    printf("mtu of %s=%d\t hwdaddrs=%02X:%02X:%02X:%02x:%02x:%02x\n",ifc[count].name,ifc[count].mtu,
+    ifc[count].hwaddr[0], ifc[count].hwaddr[1], ifc[count].hwaddr[2],
+    ifc[count].hwaddr[3], ifc[count].hwaddr[4], ifc[count].hwaddr[5]);
    } 
  }
  
  else 
  {
- 	
-   //while(ifr[--total_interfaces].ifr_name != argv[1])  
-   while (strcmp(ifr[--total_interfaces].ifr_name, argv[1]))
-   {     
-      //printf("name=%s, argv[1]=%s\n",ifr[total_interfaces].ifr_name, argv[1]);
-      continue;
-   }
-   
-   printf("mtu of %s=%d\t hwdaddrs=%02X:%02X:%02X:%02x:%02x:%02x\n",ifr[total_interfaces].ifr_name,ifr[total_interfaces].ifr_mtu,
-    (unsigned char)ifr[total_interfaces].ifr_hwaddr.sa_data[0], (unsigned char)ifr[total_interfaces].ifr_hwaddr.sa_data[1], (unsigned char)ifr[total_interfaces].ifr_hwaddr.sa_data[2],
-    (unsigned char)ifr[total_interfaces].ifr_hwaddr.sa_data[3], (unsigned char)ifr[total_interfaces].ifr_hwaddr.sa_data[4], (unsigned char)ifr[total_interfaces].ifr_hwaddr.sa_data[5]);
-   }
+ 	   count=0;
+	   strncpy(ifc[count].name, port_name, IFNAMSIZ);
+	   
+	   printf("mtu of %s=%d\t hwdaddrs=%02X:%02X:%02X:%02x:%02x:%02x\n",ifc[count].name,ifc[count].mtu,
+	   ifc[count].hwaddr[0], ifc[count].hwaddr[1], ifc[count].hwaddr[2],
+	   ifc[count].hwaddr[3], ifc[count].hwaddr[4], ifc[count].hwaddr[5]);
+  }
 
 }
  
+
  
  
- 
- void Get_all_interfaces_names(struct ifreq *ifr, short int total_interfaces)
+ void get_all_interfaces_names(struct interfaces *ifc, short int count, unsigned char *interface_names)
  {
  	short int i=0;
- 	for (total_interfaces; total_interfaces >= 0; --total_interfaces)
+ 	for (count; count >= 0; count--)
 	{ 
 	 short int pos=0;
-	  while(path[i] != '\n')
+	  while(interface_names[i] != '\n')
 	  { 	 
-			 ifr[total_interfaces].ifr_name[pos++] = path[i];
+			 ifc[count].name[pos++] = interface_names[i];
 			 i++;
-			 if(path[i] == '\n') 
+			 if(interface_names[i] == '\n') 
 			 {
-			 	   //Get_info(ifr[total_interfaces].ifr_name, fd);
-				 	//printf("ifr=%s\n",ifr[total_interfaces].ifr_name);
+			 	   ifc[count].name[pos] = '\0';
 				 	i++;
 				 	break; 
 			 }
