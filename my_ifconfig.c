99a2a648b28967709c0c638528f5ab21079b4bfe
Swapnil Kulkarni <swapnilvamankulkarni@gmail.com>
2021-05-22 21:27:54 +0530

    close to final code

--- my_ifconfig.c
+++ my_ifconfig.c
@@ -6,6 +6,7 @@
 #include <sys/ioctl.h>
 
 #define MAC_SIZE 6
+#define MAX_INTERFACES 5
 
 
 
@@ -18,7 +19,7 @@ int mtu;
 };
 
 static void get_all_interfaces_names(struct interfaces *ifc, short int count, unsigned char *interface_names);
-static int get_all_interfaces_info(struct interfaces *ifc, struct ifreq *ifr, unsigned char *port_name, int count, int fd);
+static int get_interfaces_info(struct interfaces *ifc, struct ifreq *ifr, unsigned char *port_name, int count, int fd);
 static void print_info(struct interfaces *ifc, unsigned char *port_name, int count);
 static int interface_info(unsigned char *port_name);
 
@@ -43,41 +44,42 @@ int main(int argc, char *argv[])
  static int interface_info(unsigned char *port_name)
 {
 	
+	struct interfaces ifc[MAX_INTERFACES]; struct ifreq ifr[MAX_INTERFACES];
  	int fd, count, goterr=0, retval=0;
 	unsigned char interface_count, interface_names[100];
 	FILE *fp, *fp2;
 	
-	
 	fp=popen("ls /sys/class/net", "r"); //to get names of the network interfaces present in the system
-	if(fp == NULL) { perror("couldnt get interface names\t"); pclose(fp); return 1; }  
+	if(fp == NULL) { perror("couldnt get interface names\t"); goterr=1; goto _close1; }  
 	 
 	fread(interface_names, sizeof(char), 100, fp);  
-	if(ferror(fp)) { printf("Error in reading interface names..!!!\n"); pclose(fp); return 1; }
+	if(ferror(fp)) { printf("Error in reading interface names..!!!\n"); goterr=1; goto _close1; }
 
 	fp2 = popen("ls /sys/class/net | wc -l", "r");  //to get total count of the network interfaces present in the system 
-	if(fp2 == NULL) { perror("couldnt get total interface count\t"); pclose(fp); pclose(fp2); return 1; }
+	if(fp2 == NULL) { perror("couldnt get total interface count\t"); goterr=1; goto _close2; }
 	
 	fread(&interface_count, sizeof(char), 1, fp2);
-	if(ferror(fp2)) { printf("Error in getting total interface count..!!!\n"); pclose(fp); pclose(fp2); return 1; }
+	if(ferror(fp2)) { printf("Error in getting total interface count..!!!\n"); goterr=1; goto _close2; }
 
    count = interface_count - 48; // converting char to int
-   struct interfaces ifc[count]; struct ifreq ifr[count];
    count = count-1; //as array index starts from 0
 
-	fd = socket(AF_INET, SOCK_DGRAM, 0); if(fd < 0) { perror("EXITING the program..!!couldnt open socket\t"); goterr=1; goto _close; }
+	fd = socket(AF_INET, SOCK_DGRAM, 0); if(fd < 0) { perror("EXITING the program..!!couldnt open socket\t"); goterr=1; goto _close3; }
    
    get_all_interfaces_names(ifc, count, interface_names);
    
-   retval = get_all_interfaces_info(ifc, ifr, port_name, interface_count-48, fd);
+   retval = get_interfaces_info(ifc, ifr, port_name, count, fd);
    
-   if(retval) { printf("Error in getting interface info...!!!\n"); goterr=1; goto _close; }
+   if(retval) { printf("Error in getting interface info...!!!\n"); goterr=1; goto _close3; }
    
-   print_info(ifc, port_name, interface_count-48);
+   print_info(ifc, port_name, count);
    
-_close:     
-      pclose(fp);
-      pclose(fp2);
-      close(fd);
+_close3:      
+   close(fd);
+_close2:   
+   pclose(fp2);
+_close1:   
+   pclose(fp);
       
    if(goterr) { return 1; } 
    else { return 0; } 
@@ -86,10 +88,10 @@ _close:
 
 /**************************************************************************************************************/
  
- static int get_all_interfaces_info(struct interfaces *ifc, struct ifreq *ifr, unsigned char *port_name, int count, int fd)
+ static int get_interfaces_info(struct interfaces *ifc, struct ifreq *ifr, unsigned char *port_name, int count, int fd)
  {
  	 int result;
- 	 count = count-1;
+ 	 
     result = strcmp(port_name, "no_port");
 	 if(result != 0) { count = 0; strncpy(ifc[count].name, port_name, IFNAMSIZ); }
 	
@@ -113,18 +115,17 @@ static void print_info(struct interfaces *ifc, unsigned char *port_name, int cou
 {
    int result;	
    
-	count = count-1;
 	result = strcmp(port_name, "no_port");
 	if(result != 0) { count = 0; strncpy(ifc[count].name, port_name, IFNAMSIZ); }
 
    printf("\tNAME\t\tMTU\t\tHWADDR\n\n");   
   
-     for(count; count >= 0; count--)
-     {   
-        printf("\t%s\t\t%d\t\t%02X:%02X:%02X:%02x:%02x:%02x\n",ifc[count].name, ifc[count].mtu,
-              ifc[count].hwaddr[0], ifc[count].hwaddr[1], ifc[count].hwaddr[2],
-              ifc[count].hwaddr[3], ifc[count].hwaddr[4], ifc[count].hwaddr[5]);
-     } 
+   for(count; count >= 0; count--)
+   {   
+      printf("\t%s\t\t%d\t\t%02X:%02X:%02X:%02x:%02x:%02x\n",ifc[count].name, ifc[count].mtu,
+             ifc[count].hwaddr[0], ifc[count].hwaddr[1], ifc[count].hwaddr[2],
+             ifc[count].hwaddr[3], ifc[count].hwaddr[4], ifc[count].hwaddr[5]);
+   } 
 
 }
  
